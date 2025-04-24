e0ce800a6bd4683ba23d7170a0cd60a968b4f5c1
Davronbek Atadjanov <atadjanovdavronbek863@gmail.com>
2025-04-24 16:57:59 +0500

    4 va 5 darslar, Barcha sahifalar

---
+++ app/(root)/contact/page.tsx
@@ -0,0 +1,67 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Textarea } from '@/components/ui/textarea'
+import { Dot, Home, Mail, Phone, Send } from 'lucide-react'
+import Link from 'next/link'
+
+const ContactPage = () => {
+	return (
+		<div className='max-w-6xl mx-auto'>
+			<div className='relative min-h-[40vh] flex items-center justify-center flex-col'>
+				<h2 className='text-center text-4xl section-title font-creteRound'>
+					<span>Contact</span>
+				</h2>
+
+				<div className='flex gap-1 items-center mt-4'>
+					<Home className='w-4 h-4' />
+					<Link
+						href={'/'}
+						className='opacity-90 hover:underline hover:opacity-100'
+					>
+						Home
+					</Link>
+					<Dot />
+					<p className='text-muted-foreground'>Contact</p>
+				</div>
+			</div>
+
+			<div className='grid grid-cols-2 max-md:grid-cols-1 gap-4 mt-6'>
+				<div className='flex flex-col'>
+					<h1 className='text-4xl font-creteRound'>Contact Donni</h1>
+					<p className='mt-2 text-muted-foreground'>
+						I am here to help and answer any questions you might have. I look
+						forward to hearing from you.
+					</p>
+
+					<div className='mt-12 flex items-center gap-3'>
+						<Mail className='w-4 h-4' />
+						<p className='text-sm'>info@donni.uz</p>
+					</div>
+
+					<div className='flex items-center gap-3 mt-2'>
+						<Phone className='w-4 h-4' />
+						<p className='text-sm'>+998 99 888 77 66</p>
+					</div>
+				</div>
+
+				<div>
+					<h1 className='text-4xl font-creteRound mb-2'>Contact Form</h1>
+					<div className='flex flex-col space-y-3'>
+						<Textarea
+							className='resize-none h-32'
+							placeholder='Ask questions or just say Hi'
+						/>
+						<Input placeholder='Email address' />
+						<Input placeholder='Your name here' />
+						<Button className='w-fit' size={'lg'}>
+							<span>Send</span>
+							<Send className='w-4 h-4 ml-2' />
+						</Button>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default ContactPage
